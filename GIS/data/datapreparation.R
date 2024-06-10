9b8e06af366c63d7c1a530777b1a4a95de8850fd
Rocco Paolillo <36846038+RoccoPaolillo@users.noreply.github.com>
2024-06-10 03:19:44 +0200

    hypertension

--- GIS/data/datapreparation.R
+++ GIS/data/datapreparation.R
@@ -2,6 +2,7 @@ library(tidyr)
 library(purrr)
 library(dplyr)
 
+# sociodemographics
 
 setwd("C:/Users/rocpa/OneDrive/Documenti/GitHub/IPF_multidim/GIS/data/lazio_ASL_istat/sociodem_asl")
 file.list = list.files( pattern = "*.csv")
@@ -20,3 +21,27 @@ df$sumgenASL <- df$femASL + df$manASL
 
 dftoteta <- df %>% group_by(DENOMINAZI) %>% summarize(sumageASL = sum(age))
 df <- merge(df,dftoteta,by = c("DENOMINAZI"))
+
+# hypertension
+
+# sociodemographics
+
+setwd("C:/Users/rocpa/OneDrive/Documenti/GitHub/IPF_multidim/GIS/data/lazio_ASL_istat/hypertension/")
+dfhp <- read.csv("F0_4.csv",sep =";",skip = 9)
+dfhp2 <- read.csv("M40_44.csv",sep =";",skip = 9) 
+dfhp3 <- read.csv2("M80_84.csv",sep =";",skip = 9) 
+dfhp4 <- read.csv2("F80_84.csv",sep =";",skip = 1) 
+dfhp5 <- read.csv("F5_9.csv",sep =";",skip = 4)
+
+
+library(stringr)
+
+file.list = list.files( pattern = "*.csv")
+theData_list<-lapply(file.list, read.csv)
+dfh <-bind_rows(theData_list)
+
+
+
+
+
+
