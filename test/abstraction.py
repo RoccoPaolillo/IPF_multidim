d813535b03ab2386f69f0b7d28f8396225e7995a
Rocco Paolillo <36846038+RoccoPaolillo@users.noreply.github.com>
2025-07-11 16:29:46 +0200

    Create abstraction.py

---
+++ test/abstraction.py
@@ -0,0 +1,21 @@
+total = 6333024
+TGT0_30 =  1745215   
+TGT30_60 = 2832088
+TGT60_100 = 1755721
+TGTmale = 3073047   
+TGTfemale = 3259977  
+TGTHPT = 1193445 # empirical target
+TGTNOHPT = 5139579
+TGTHF = 93926 # empirical target
+TGTNOHF = 6239098
+####
+TGT0_30HPT = 3547   # empirical target
+TGT30_60HPT = 252543  # empirical target
+TGT60_100HPT = 937355  # empirical target
+TGTmaleHPT = 579109 # synthetic target
+TGTfemaleHPT = 614336 # synthetic target
+TGT0_30HF = 424  # empirical target
+TGT30_60HF = 8459  # empirical target
+TGT60_100HF = 85043  # empirical target
+TGTmaleHF = 45577 # synthetic target
+TGTfemaleHF = 48349 # synthetic target
